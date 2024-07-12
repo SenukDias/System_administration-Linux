9661f4ee8262e198c0febd02b236053fb7fba9f5
Senuk Dias <senukdias@gmail.com>
2024-07-12 15:47:34 +0530

    new

--- Tasks/Automation/Crontab/bk_web.sh
+++ Tasks/Automation/Crontab/bk_web.sh
@@ -2,9 +2,10 @@
 
 tar -czf /tmp/html_$(date +%d-%m).tar.gz /var/www/html
 
-scp -r /tmp/html_$(date +%d-%m).tar.gz root@192.168.125.130:/backup/daily/Web/day-$(date +%d-%m).tar.gz
+scp -r /tmp/html_$(date +%d-%m).tar.gz root@192.168.125.130:/backup/daily/Web/day-$(date +%d-%m-%M).tar.gz
 
 rm /tmp/html_$(date +%d-%m).tar.gz
+
 
 
 # 30 12 * * *  /path/to/script/bk_web.sh 
